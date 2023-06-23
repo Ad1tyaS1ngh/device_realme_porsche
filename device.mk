0d9e4ec3c6d2c98b1f212a82daf484a3b55744fa
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-06-24 04:53:02 +0530

    porsche: Convert all overlays to RROs
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- device.mk
+++ device.mk
@@ -304,11 +304,12 @@ PRODUCT_PACKAGES += \
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-aosp
-
-PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
+    AOSPPorscheApertureOverlay \
+    AOSPPorscheDozeOverlay \
+    AOSPPorscheFrameworksOverlay \
+    AOSPPorscheSettingsOverlay \
+    AOSPPorscheSystemUIOverlay \
     PorscheCarrierConfigOverlay \
     PorscheFrameworksOverlay \
     PorscheOPlusFrameworksOverlay \
