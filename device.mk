e698543f0fd62cc7108e7ea9a4192b28be3ae218
ad1tyas1ngh <adityasingh1768@gmail.com>
2023-07-31 04:09:01 +0530

    porsche: sensors: Rebrand to porsche
    Signed-off-by: ad1tyas1ngh <adityasingh1768@gmail.com>

--- device.mk
+++ device.mk
@@ -352,7 +352,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge \
-    sensors.oplus
+    sensors.porsche
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
