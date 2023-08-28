6373dc8a74e3ac12c8cba314e9d3dc19f43435b6
Aditya Singh <adityasingh1768@gmail.com>
2023-08-28 18:46:04 +0000

    porsche: Set ro.boot.hardware.sku from libinit
    * Setting it from target rc doesn't seem to work
    
    Signed-off-by: Aditya Singh <adityasingh1768@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,6 +82,9 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH)/configs/hidl/manifest_odm.xml
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_porsche
+
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 3
 BOARD_KERNEL_BASE := 0x00000000
